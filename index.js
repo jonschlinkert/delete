29753d2fc92e2de2b59936f01251eeed9bd1350c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-28 01:29:11 -0400

    clean up

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
 /*!
  * delete <https://github.com/jonschlinkert/delete>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
-var isPathCwd = require('is-path-cwd');
-var isPathInCwd = require('is-path-in-cwd');
+var path = require('path');
 var rimraf = require('rimraf');
 
-function safeCheck(filepath) {
-  if (isPathCwd(filepath)) {
-    throw new Error('Cannot delete the current working directory. Can be overriden with the `force` option.');
+module.exports = function (fp, options, next) {
+  if (typeof options === 'function') {
+    next = options; options = {};
   }
+  fp = fp.split('\\').join('/');
+  if (!(options && options.force)) {
+    safeCheck(fp);
+  }
+  rimraf(fp, next);
+};
 
-  if (!isPathInCwd(filepath)) {
-    throw new Error('Cannot delete files/folders outside the current working directory. Can be overriden with the `force` option.');
+module.exports.sync = function (fp, options) {
+  if (!(options && options.force)) {
+    safeCheck(fp);
   }
+  fp = fp.split('\\').join('/');
+  return rimraf.sync(fp);
+};
+
+function stripSlash(fp) {
+  if (fp.substr(-1) === '/') {
+    return fp.slice(0, -1);
+  }
+  return fp
 }
 
-module.exports = function (filepath, options, next) {
-  if (typeof options === 'function') {
-    next = options;
-    options = {};
-  }
+function isCurrentDir(fp) {
+  return process.cwd() === path.resolve(fp)
+    || fp === './'
+    || fp === '.';
+}
 
-  if (!(options && options.force)) {
-    safeCheck(filepath);
+function insideCurrentDir(fp) {
+  var cwd = stripSlash(process.cwd());
+  fp = stripSlash(path.resolve(fp));
+  if (path.sep === '\\') {
+    cwd = cwd.toLowerCase();
+    fp = fp.toLowerCase();
   }
-  return rimraf(filepath, next);
-};
+  return fp.substr(0, cwd.length) === cwd;
+}
 
-
-module.exports.sync = function (filepath, options) {
-  if (!(options && options.force)) {
-    safeCheck(filepath);
+function safeCheck(fp) {
+  var force = 'Can be overriden with the `force` option.';
+  if (isCurrentDir(fp)) {
+    throw new Error('Cannot delete the current working directory. ' + force);
   }
-  return rimraf.sync(filepath);
-};
+  if (!insideCurrentDir(fp)) {
+    throw new Error('Cannot delete files/folders outside the current working directory. ' + force);
+  }
+}
