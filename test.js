8f3c9bcaa703fd7b14b7c8bf1a389b87cb267074
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-09 10:34:50 -0400

    update tests

--- test.js
+++ test.js
@@ -9,34 +9,92 @@
 require('should');
 var fs = require('fs');
 var del = require('./');
+var Promise = require('bluebird');
 
 describe('delete:', function () {
-  beforeEach(function (cb) {
-    fs.writeFile('a.txt', 'This is a test.', function (err) {
-      if (err) console.log(err);
-      cb();
-    });
-  });
-
   describe('async:', function () {
-    it('should delete files asynchronously.', function (cb) {
-      fs.existsSync('a.txt').should.be.true;
+    beforeEach(function (done) {
+      fs.writeFile('a.txt', 'This is a test.', function (err) {
+        if (err) return done(err);
+        done();
+      });
+    });
 
-      del('a.txt', function(err) {
-        if (err) console.log(err);
-        fs.existsSync('a.txt').should.be.false;
-        cb();
+    it('should delete files asynchronously.', function (done) {
+      fs.exists('a.txt', function (exists) {
+        exists.should.be.true;
+
+        del('a.txt', function(err) {
+          if (err) return done(err);
+          fs.existsSync('a.txt').should.be.false;
+          done();
+        });
+      });
+    });
+
+    it('should not delete the cwd:', function (done) {
+      del('.', function(err) {
+        if (err) err.message.should.equal('Whoooaaa there! If you\'re sure you want to do this, define `options.force` to delete the current working directory.');
+        done();
+      });
+    });
+
+    it('should not delete parent directories:', function (done) {
+      del('../', function(err) {
+        if (err) err.message.should.equal('Yikes!! Take care! `options.force` must be defined to delete files or folders outside the current working directory.');
+        done();
       });
     });
   });
 
   describe('sync:', function () {
     it('should delete files synchronously.', function () {
-      var fixture = 'a.txt';
-      fs.existsSync(fixture).should.be.true;
+      fs.writeFileSync('a.txt', 'This is a test.');
+      fs.existsSync('a.txt').should.be.true;
 
       del.sync('a.txt');
-      fs.existsSync(fixture).should.be.false;
+      fs.existsSync('a.txt').should.be.false;
+    });
+
+    it('should not delete the current working directory.', function () {
+      (function () {
+        del.sync('.');
+      }).should.throw('Whoooaaa there! If you\'re sure you want to do this, define `options.force` to delete the current working directory.');
+    });
+
+    it('should not delete parent directories.', function () {
+      (function () {
+        del.sync('../');
+      }).should.throw('Yikes!! Take care! `options.force` must be defined to delete files or folders outside the current working directory.');
+    });
+  });
+
+  describe('promise:', function () {
+    beforeEach(function (done) {
+      fs.writeFile('a.txt', 'This is a test.', function (err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    afterEach(function (done) {
+      fs.exists('a.txt', function (exists) {
+        exists.should.be.false;
+        done();
+      });
+    });
+
+    it('should delete files with `del.promise`.', function (done) {
+      fs.exists('a.txt', function (exists) {
+        exists.should.be.true;
+        del.promise('a.txt')
+          .then(function () {
+            fs.exists('a.txt', function (exists) {
+              exists.should.be.false;
+              done();
+            });
+          });
+      });
     });
   });
 });
