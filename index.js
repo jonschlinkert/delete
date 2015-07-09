3f39ae394b0f7d8747d908f5e159c0060e0fd791
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-09 10:34:46 -0400

    adds promise method

--- index.js
+++ index.js
@@ -7,57 +7,64 @@
 
 'use strict';
 
-var path = require('path');
-var rimraf = require('rimraf');
+var lazy = require('lazy-cache')(require);
+var rimraf = lazy('rimraf');
+var promise = lazy('bluebird');
+var utils = require('./lib/utils');
 
-module.exports = function (fp, options, next) {
+function del(fp, options, next) {
   if (typeof options === 'function') {
-    next = options; options = {};
+    next = options;
+    options = {};
   }
-  fp = fp.split('\\').join('/');
-  if (!(options && options.force)) {
-    safeCheck(fp);
+  try {
+    assertDirectory(fp, options);
+    return rimraf()(fp, next);
+  } catch(err) {
+    return next(err);
   }
-  rimraf(fp, next);
+}
+
+
+del.sync = function delSync(fp, options) {
+  try {
+    assertDirectory(fp, options);
+    rimraf().sync(fp);
+    return fp;
+  } catch (err) {
+    throw err;
+  }
 };
 
-module.exports.sync = function (fp, options) {
-  if (!(options && options.force)) {
-    safeCheck(fp);
-  }
-  fp = fp.split('\\').join('/');
-  return rimraf.sync(fp);
+
+del.promise = function delPromise(fp, options) {
+  assertDirectory(fp, options);
+  var Promise = promise();
+
+  return new Promise(function (resolve, reject) {
+    rimraf()(fp, function (err) {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 };
 
-function stripSlash(fp) {
-  if (fp.substr(-1) === '/') {
-    return fp.slice(0, -1);
+
+function assertDirectory(fp, options) {
+  options = options || {};
+  if (options && options.force) return;
+
+  if (utils.isCurrentDir(fp) === true) {
+    throw new Error('Whoooaaa there! If you\'re sure you want to do this, define `options.force` to delete the current working directory.');
   }
-  return fp
+
+  if (utils.insideCurrentDir(fp) === false) {
+    throw new Error('Yikes!! Take care! `options.force` must be defined to delete files or folders outside the current working directory.');
+  }
 }
 
-function isCurrentDir(fp) {
-  return process.cwd() === path.resolve(fp)
-    || fp === './'
-    || fp === '.';
-}
+/**
+ * Expose `del`
+ */
 
-function insideCurrentDir(fp) {
-  var cwd = stripSlash(process.cwd());
-  fp = stripSlash(path.resolve(fp));
-  if (path.sep === '\\') {
-    cwd = cwd.toLowerCase();
-    fp = fp.toLowerCase();
-  }
-  return fp.substr(0, cwd.length) === cwd;
-}
-
-function safeCheck(fp) {
-  var force = 'Can be overriden with the `force` option.';
-  if (isCurrentDir(fp)) {
-    throw new Error('Cannot delete the current working directory. ' + force);
-  }
-  if (!insideCurrentDir(fp)) {
-    throw new Error('Cannot delete files/folders outside the current working directory. ' + force);
-  }
-}
+module.exports = del;
